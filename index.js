c11ac67e420f4e1fe56bdbda61ebb105ba3dba8e
Diana Gitelman <diana4kany@gmail.com>
2022-04-26 19:51:03 -0400

    commit tree 0e7dd98e6c6d7da9f7e310ace5919b40641b7a63

---
+++ index.js
@@ -0,0 +1,12 @@
+function receivesAFunction(callback){
+    callback();
+}
+function returnsANamedFunction() {
+    return function spy(){
+
+    }
+}
+function returnsAnAnonymousFunction() {
+    return function(){
+    }
+}
